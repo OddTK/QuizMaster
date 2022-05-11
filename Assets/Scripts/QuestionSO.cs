14d5435e614efcac35b9a52f5a5f499025d2facd
Jesus <jesusbalandra@outlook.com>
2022-05-11 09:51:47 -0600

    created questions

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -5,5 +5,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionSO : ScriptableObject
 {
-    string question = "Enter new question text here";
+    // TextArea in this instance makes our text box for our question bigger
+    // Takes 2 params: min number of lines, max number of lines
+    [TextArea(2,6)]
+    [SerializeField] string question = "Enter new question text here";
 }
