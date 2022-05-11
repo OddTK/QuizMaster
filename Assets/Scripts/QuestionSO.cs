95e4f80b3a0e5e5b1beca088ef9599b6ae728836
Jesus <jesusbalandra@outlook.com>
2022-05-11 12:36:34 -0600

    created question answers

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -9,4 +9,22 @@ public class QuestionSO : ScriptableObject
     // Takes 2 params: min number of lines, max number of lines
     [TextArea(2,6)]
     [SerializeField] string question = "Enter new question text here";
+    // this will create a SerializeField of string array called answers that will have 4 elements
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
+    
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
 }
