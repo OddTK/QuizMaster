26835029d1a6f580ca5f0d4b9e5bf8ed9bb0e623
Jesus <jesusbalandra@outlook.com>
2022-05-12 11:21:45 -0600

    linked timer image and button for countdown

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -54,6 +54,5 @@ public class Timer : MonoBehaviour
                 loadNextQuestion = true;
             }
         }
-        Debug.Log(isAnsweringQuestion + ": " + timerValue + " = " + fillFraction);
     }
 }
