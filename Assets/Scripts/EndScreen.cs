7ec42782cdc2cfe9c0c98e31a645eb0bf3e3763f
Jesus <jesusbalandra@outlook.com>
2022-05-12 12:20:02 -0600

    finished endScreen

--- Assets/Scripts/EndScreen.cs
+++ Assets/Scripts/EndScreen.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class EndScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] TextMeshProUGUI finalScoreText;
+    ScoreKeeper scoreKeeper;
     void Start()
     {
-        
+        scoreKeeper = FindObjectOfType<ScoreKeeper>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ShowFinalScore()
     {
-        
+        finalScoreText.text = "Congratulations!\nYou got a score of " + scoreKeeper.CalculateScore() + "%";
     }
 }
